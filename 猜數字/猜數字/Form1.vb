8840aaa2e1f4529142159ab37013d63dfa7b77cf
Ching-Chang Lin <cclin@im.tpcu.edu.tw>
2017-03-28 20:07:21 +0800

    Signed-off-by: Ching-Chang Lin <cclin@im.tpcu.edu.tw>

---
+++ 猜數字/猜數字/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Randomize()
+        Dim target As Double = Rnd()
+        Dim guess As Integer
+        Label1.Text = target
+        target = CInt(Int((100 * target) + 1))
+        Label2.Text = target
+        Do
+            guess = Val(InputBox("請輸入一數字"))
+            If (target > guess) Then
+                MsgBox("猜大一點")
+            ElseIf (target < guess) Then
+                MsgBox("猜小一點")
+            Else
+                MsgBox("猜中了")
+            End If
+        Loop While (target <> guess)
+    End Sub
+End Class
---
+++ 質數判斷/質數判斷/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim Flag As Integer = 1
+        Dim i As Integer = 2
+        Do While ((Flag = 1) And (i < Val(TextBox1.Text)))
+            If ((Val(TextBox1.Text) Mod i) = 0) Then
+                Flag = 0
+            End If
+            i = i + 1
+        Loop
+        If (Flag = 1) Then
+            TextBox2.Text = TextBox1.Text & "為質數"
+        Else
+            TextBox2.Text = TextBox1.Text & "不為質數"
+        End If
+    End Sub
+End Class
